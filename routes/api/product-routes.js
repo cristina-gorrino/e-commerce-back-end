672b5ba268cbca1661f8042dd0e9554ab47304a9
cgorrino <cgorrino03@gmail.com>
2021-06-07 23:51:23 -0700

    Adding route to get one Product by id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -22,6 +22,14 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+  const productData = await Product.findByPk(req.params.id, {
+  include: [{model: Category}, {model: Tag}],
+});
+  res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
