9e8e43ba65eb4962dcbcb1ea45383f344aaaac71
cgorrino <cgorrino03@gmail.com>
2021-06-08 23:11:06 -0700

    Fixing bug in delete tag route. Adding successful response.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -64,6 +64,7 @@ router.delete('/:id', async (req, res) => {
     if (!tagData) {
       res.status(404).json({ message: 'No tag found with that id'});
       return;
+      res.status(200).json(tagData);
     }
 
   } catch (err) {
