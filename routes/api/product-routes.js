96b228772182fa5f13dcee63811502a010322a1e
cgorrino <cgorrino03@gmail.com>
2021-06-08 22:57:13 -0700

    Fixing bug with delete product route. Making sure success returns the right status

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -117,6 +117,7 @@ router.delete('/:id', async (req, res) => {
     if (!productData) {
       res.status(404).json({ message: 'No product found with that id'});
       return;
+      res.status(200).json(product);
     }
 
   } catch (err) {
