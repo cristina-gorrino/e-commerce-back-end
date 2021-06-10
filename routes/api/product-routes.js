a0182c495f177844cfafe6133b8ca29ae45596eb
cgorrino <cgorrino03@gmail.com>
2021-06-09 22:23:08 -0700

    Fixing 500 error on delete product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -117,8 +117,8 @@ router.delete('/:id', async (req, res) => {
     if (!productData) {
       res.status(404).json({ message: 'No product found with that id'});
       return;
-    }
-    res.status(200).json(product);
+    } 
+      res.status(200).json(productData);
 
   } catch (err) {
     res.status(500).json(err);
